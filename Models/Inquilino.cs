8fc1bf408f0be16868599070fdd6762b76f04047
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2026-08-20 17:56:55 -0300

    renombrar Id a Id_inquilino y cambiar Estado a tipo boolean

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -2,13 +2,13 @@ namespace Inmobiliaria.Models;
 
 public class Inquilino
 {
-    public int Id { get; set; }
+    public int Id_inquilino { get; set; }
     public string Dni { get; set; } = ""; 
     public string Nombre { get; set; } = "";
     public string Apellido { get; set; } = "";
     public string Telefono { get; set; } = "";
     public string Email { get; set; } = "";
-    public string Estado { get; set; } = "";
+    public bool Estado { get; set; }
 
     public override string ToString()
     {
